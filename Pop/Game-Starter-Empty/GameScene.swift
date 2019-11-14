278e37159cc90922b97ed8240f0dd058b39a89e9
mohammed <mdrame113@gmail.com>
2019-11-13 18:28:10 -0800

    done

---
+++ Pop/Game-Starter-Empty/GameScene.swift
@@ -0,0 +1,115 @@
+//
+//
+//  Created by Mo on 9/29/19.
+//  Copyright © 2019 Mo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+  
+    var score = 0
+    
+    //properties of the scoreLabel
+    var scoreLabel: SKLabelNode = {
+        let label = SKLabelNode()
+        label.text = "0"
+        label.color = .white
+        label.fontSize = 50
+        
+        return label
+    }()
+    
+    override func didMove(to view: SKView) {
+        //Called when the scene has been displayed
+        
+       
+        createSquares(name: "one")
+        createSquares(name: "two")
+        createSquares(name: "three")
+
+        
+        labelSetUp()
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+    
+    func labelSetUp() {
+        scoreLabel.position.x = view!.bounds.width / 2
+        scoreLabel.position.y = view!.bounds.height - 80
+        addChild(scoreLabel)
+    }
+    
+    func randomNumber()-> CGFloat {
+        //Width of the SKScene's view
+        let viewsWidth = self.view!.bounds.width
+        //Creates a random number from 0 to the viewsWidth
+        let randomNumber = CGFloat.random(in: 0 ... viewsWidth)
+        
+        return randomNumber
+    }
+    
+    func createSquares(name: String) {
+        //Set up square properties
+        //1. Create a CGSize for the square with (width: 80, height: 80)
+        let squareDimensions = CGSize(width: 80, height: 80)
+        //2. Create a Square node with a texture of nil. a color of .green and the size we created above
+        let squareNode = SKSpriteNode(texture: nil, color: .purple, size: squareDimensions)
+        //3. Set the squares name to the name we pass into this function
+        squareNode.name = name
+        
+     
+        //1. Squares y positions shoud start at 40
+        squareNode.position.y = 40
+        //2. Squares x positon should use the randomNumber generator provided above
+        squareNode.position.x = randomNumber()
+
+        //Create an action to move the square up the screen
+        let action = SKAction.customAction(withDuration: 2.0, actionBlock: { (square, _) in
+            //Set up the squares animation
+            //1. The squares y position should increase by 10
+            squareNode.position.y += 10
+            //2. Create an if statement that checks if the squares y position is >= to the screens height
+            if squareNode.position.y >= self.view!.frame.height {
+                //If it is remove the square and create a new square with the same name
+                squareNode.removeFromParent()
+                self.createSquares(name: squareNode.name!)
+            }
+        })
+        
+        //Have the square run the above animation forever and add the square to the SKScene!
+        addChild(squareNode)
+        squareNode.run(SKAction.repeatForever(action))
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //Loop through an array of touch values
+        for touch in touches {
+            
+            //Grab the position of that touch value
+            let positionInScene = touch.location(in: self)
+            
+            //Find the name for the node in that location
+            //Note: Changed from name to node. This var used to have .name at the end
+            let node = self.atPoint(positionInScene)
+            
+            //Check if there is an node there.
+            if node.name != nil {
+                //Remove the square
+                //Remove node from parent view
+                node.removeFromParent()
+                //Increase the score by one
+                score += 1
+                scoreLabel.text = ("\(score)")
+                //Create the square again with the same name
+                createSquares(name: node.name!)
+            }
+        }
+    }
+    
+}
